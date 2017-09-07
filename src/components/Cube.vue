2f976612ae47fcff6bbbb50b650c2fc9a9e4b7d8
Alex Tou <atou@marinedepot.com>
2017-09-07 14:40:29 -0700

    git init

---
+++ src/components/Cube.vue
@@ -0,0 +1,39 @@
+<template>
+  <div id="cube">
+    <img src="//www.f3images.com/IMD/MD_images/Calculators/Sand_Calculator_Images/Sand_Square.jpg" class="img-responsive center-block" alt="">
+    <div class="form">
+      <label>Length</label>
+      <input type="text" id="firstNumber" v-model="firstNumber">
+    </div>
+    <p v-if="result==='0.00'">Please enter a Length.</p>
+    <p v-else-if="result==='NaN'">Invalid Length</p>
+    <p v-else>{{ result }} Gallons</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'cube',
+  data:  function() {
+    return {
+      firstNumber: '',
+    };
+  },
+  computed: {
+    result: function() {
+      return ((this.firstNumber*this.firstNumber*this.firstNumber)*0.0043290).toFixed(2);
+    }
+  },
+};
+</script>
+<!-- styling for the component -->
+<style>
+#cube {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/main.js
@@ -0,0 +1,34 @@
+import Vue from 'vue'
+import App from './App.vue'
+import VueRouter from 'vue-router'
+
+Vue.use(VueRouter)
+
+import Rectangle from './components/Rectangle.vue'
+import Cube from './components/Cube.vue'
+
+const routes = [
+{ path: '/', component: Rectangle },
+{ path: '/cube', component: Cube }
+]
+
+const router = new VueRouter({
+  routes, // short for routes: routes
+  mode: 'history'
+})
+
+new Vue({
+//define the selector for the root component
+  el: '#app',
+  //pass the template to the root component
+  template: '<App/>',
+  //declare components that the root component can access
+  components: { App },
+  //pass in the router to the Vue instance
+  router
+}).$mount('#app')//mount the router on the app
+/*
+new Vue({
+  el: '#app',
+  render: h => h(App)
+})*/
